2a085ac2f45059d01db1d3b627bdd15cf15dcd6e
robertoneves <robertonevessilva#gmail.comm>
2026-03-12 21:19:41 -0300

    feat: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const corsOrigins = (process.env.CORS_ORIGIN || 'http://localhost:9000')
+  const corsOrigins = (process.env.CORS_ORIGIN || 'https://agenda-front-1i78.onrender.com,http://localhost:9000')
     .split(',')
     .map((origin) => origin.trim())
     .filter(Boolean);
